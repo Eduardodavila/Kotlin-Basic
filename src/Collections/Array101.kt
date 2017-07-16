4b16ef10239d7a5aef16d48815dd9255ef39fa25
Eduardo D'Avila <eduardo@eduardodavila.com.br>
2017-07-16 17:29:08 -0300

    Array101

---
+++ src/Collections/Array101.kt
@@ -0,0 +1,21 @@
+package Collections
+
+fun main(args: Array<String>) {
+    val array = arrayOf("Texas", "Iowa", "California")
+    val mixed = arrayOf("Kotlin", 17, 3.0, false)
+    val numbers = intArrayOf(1,2,3,4,5,6)
+    println(mixed[0])
+    mixed[2] = "House"
+    println(mixed[2])
+    val str = "Kotlin"
+    println(str[0])
+
+    val states = arrayOf("Nevada", "Florida")
+    val allArrayMixed = array + states
+
+    println(allArrayMixed.size)
+
+
+
+
+}

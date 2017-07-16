cb49a551e11e1ee92072ed6ecd83f142dfd771ee
Eduardo D'Avila <eduardo@eduardodavila.com.br>
2017-07-15 21:33:37 -0300

    start

---
+++ src/HelloWorld.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var str1: String = "Hello World"
+    var str2: String = " Kotlin"
+    var age: Int = 45
+    var myName = "Ed"
+
+    println(str1 +  str2 + " Age= " + age)
+
+}

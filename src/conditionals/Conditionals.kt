d434ddbfce273694a1107242a734a8d951734b63
Eduardo D'Avila <eduardo@eduardodavila.com.br>
2017-07-15 22:31:38 -0300

    start

---
+++ src/conditionals/Conditionals.kt
@@ -0,0 +1,24 @@
+package conditionals
+
+fun main(args: Array<String>) {
+
+    val age: Int = 29
+
+    if (age < 18) {
+
+        println("You cannot register!")
+
+    } else if (age < 21) {
+
+        println("Well, maybe you can register!")
+
+    } else if (age < 30) {
+
+        println("You should be able to register!")
+
+    } else {
+
+        println("You're good to go!")
+    }
+    println("The end.")
+}

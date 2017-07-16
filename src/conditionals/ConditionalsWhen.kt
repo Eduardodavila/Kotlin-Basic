58a462184ea353f8ff7b0ed21e1164975d810c99
Eduardo D'Avila <eduardo@eduardodavila.com.br>
2017-07-16 11:47:47 -0300

    Conditionals When

---
+++ src/conditionals/ConditionalsWhen.kt
@@ -0,0 +1,14 @@
+package conditionals
+
+fun main(args: Array<String>) {
+    val mode: Int = 2
+
+    when (mode) {
+        1 -> println("The mode is Lazy")
+        2 -> {
+            println("The mode is 2")
+            println("So the mode is busy")
+        }
+        3 -> println("The mode is super-productive")
+    }
+}

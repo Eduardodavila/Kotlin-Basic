32c645c1bae84f4715bc95ef5e9e0e45d7b18aec
Eduardo D'Avila <eduardo@eduardodavila.com.br>
2017-07-16 17:07:35 -0300

    Collections Array List

--- src/Collections/ArraysLists.kt
+++ src/Collections/ArraysLists.kt
@@ -3,12 +3,16 @@ package Collections
 fun main(args: Array<String>) {
     val array = arrayOf("Texas", "Iowa", "California")
     val array2 = arrayOf("Text","Error", "House")
+    // array.add Not found
 
     val list = listOf("Orange","Apple","Bananas")
+    // list.add Not found
 
     val arrayList = arrayListOf("patrick", "Michael", "Sarah")
+    arrayList.add("Ice")
     println(array)
     println(array2)
     println(list)
     println(arrayList)
+
 }

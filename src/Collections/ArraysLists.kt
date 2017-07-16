cd6f8cf6eae8373f31d1280edd0a4acd0e85c8d0
Eduardo D'Avila <eduardo@eduardodavila.com.br>
2017-07-16 16:24:41 -0300

    Conditionals When Varatimos

--- src/Collections/ArraysLists.kt
+++ src/Collections/ArraysLists.kt
@@ -1,5 +1,14 @@
 package Collections
 
 fun main(args: Array<String>) {
+    val array = arrayOf("Texas", "Iowa", "California")
+    val array2 = arrayOf("Text","Error", "House")
 
+    val list = listOf("Orange","Apple","Bananas")
+
+    val arrayList = arrayListOf("patrick", "Michael", "Sarah")
+    println(array)
+    println(array2)
+    println(list)
+    println(arrayList)
 }

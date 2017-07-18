eca6372ace549e7374d0c072e64d7731bdf8ccf1
Eduardo D'Avila <eduardo@eduardodavila.com.br>
2017-07-18 18:51:27 -0300

    list101

---
+++ src/Collections/List101.kt
@@ -0,0 +1,16 @@
+package Collections
+
+fun main(args: Array<String>) {
+    val arrayList = arrayListOf("Patrick", "Michel", "Sarah")
+    val list = arrayListOf("Peter")
+    println(arrayList[0])
+    println(arrayList + list)
+    println(arrayList.size)
+    println(arrayList.isEmpty())
+    println(arrayList.contains("Sarah"))
+    arrayList.add("Josh")
+    arrayList.add(0,"Jack")
+    println(arrayList)
+    val subList = arrayList.subList(1, toIndex = 4)
+    println(subList)
+}

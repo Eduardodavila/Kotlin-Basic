b87b9e3fb14fc575a9b079c72ebb6709a02c84f3
Eduardo D'Avila <eduardo@eduardodavila.com.br>
2017-07-25 20:35:13 -0300

    functions

--- src/functions/Functions.kt
+++ src/functions/Functions.kt
@@ -1,5 +1,7 @@
 package functions
 
+import java.util.*
+
 fun helloWorld(){
     println("Hello World!")
 }
@@ -9,8 +11,20 @@ fun printWhitSpaces(text: String){
     }
    // println()
 }
+fun getCurrentDate(): Date{
+    return Date()
+}
+fun max(a: Int, b: Int): Int{
+    if (a >= b){
+        return  a
+    } else{
+        return b
+    }
+}
 
 fun main(args: Array<String>) {
     helloWorld()
-    printWhitSpaces("Kotlin")
+    printWhitSpaces("Kotlin\n")
+    println(getCurrentDate())
+    println(max(5, 1))
 }
